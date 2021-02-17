53156e76ea388f09ef62d84faa9a5a4360348ba0
Ousmane Diaw <ousmane.m.diaw@gmail.com>
2021-02-17 10:06:07 +0000

    add test specification

--- Checkout.Tests/BasketTests.cs
+++ Checkout.Tests/BasketTests.cs
@@ -19,5 +19,25 @@ namespace Checkout.Tests
             // Assert
             Assert.AreEqual(expected: 1, actual: basket.ItemsCount);
         }
+
+        [TestMethod]
+        public void Given_ItemsHaveBeenAddedToTheBasket_ThenTheTotalCostOfTheBasketShouldBeCalculated()
+        {
+            // Arrange
+            var basket = new Basket();
+            var itemA = new Item('A', 10);
+            var itemB = new Item('B', 15);
+            var itemC = new Item('C', 40);
+            var itemD = new Item('D', 55);
+
+            // Act
+            basket.AddItem(itemA);
+            basket.AddItem(itemB);
+            basket.AddItem(itemC);
+            basket.AddItem(itemD);
+
+            // Assert
+            Assert.AreEqual(expected: 120, actual: basket.TotalCost);
+        }
     }
 }
